4345d7c647a0719e7b5d0df84a983ac5a1afc4e3
Matthes Staacken <m.staacken@mbd-team.de>
2021-12-13 15:42:07 +0100

    vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  publicPath: "/todo/",
+  outputDir: "./docs",
+  //pwa: {
+  //workboxPluginMode: "InjectManifest",
+  //workboxOptions: {
+  //swSrc: "src/service-worker.js",
+  //},
+  //},
+};
